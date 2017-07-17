692a83d18e7af355cd1e3618c68856e6524d10de
tim24jones <tim.24.jones@gmail.com>
2017-07-17 13:47:03 -0400

    Update 12.py

--- Chap_9/12.py
+++ Chap_9/12.py
@@ -1,13 +1,11 @@
 def remove_all(substr,theStr):
-	count=0
-	rmStr=''
-	chngstr=theStr
-	i=0
-	while i<len(chngstr):
-		if substr==chngstr[i:i+len(substr)]:
-			rmStr=chngstr[:i]+chngstr[i+len(substr):]
-			chngstr=rmStr
-			i=i+1
-	if count==0:
-		rmStr=theStr
+    rmStr=''
+    i=0
+    while i<len(theStr):
+        if substr==theStr[i:i+len(substr)]:
+            rmStr=rmStr
+            i=i+len(substr)
+        else:
+            rmStr=rmStr+theStr[i]
+            i=i+1
     return(rmStr)
