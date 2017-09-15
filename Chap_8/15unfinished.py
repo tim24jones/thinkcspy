c386e06cb6fd5d2474f0406df5b5fa65956da478
Tim Jones <tim.24.jones@gmail.com>
2017-09-14 21:37:24 -0400

    Update 15unfinished.py

--- Chap_8/15unfinished.py
+++ Chap_8/15unfinished.py
@@ -4,16 +4,16 @@ import sys
 sys.setExecutionLimit(200000) #may run long, so be patient
 
 class matrix3_3:
-    __init__(self,row0,row1,row2):
+    def __init__(self,row0,row1,row2):
         self.r(0)=row0
         self.r(1)=row1
         self.r(2)=row2
 
-    __str__(self):
+    def __str__(self):
         return([self.r0,self.r1,self.r2])        
 
     def getcol(self,n):
-        if n>=0 and n<=2
+        if n>=0 and n<=2:
             self.c(n)=row0[n]+row1[n]+row2[n]
         else:
             return 'row or column outside matrix limits'
@@ -56,19 +56,8 @@ def convolution(filename):
                         greynearp=greyscale(nearp)
                         greypvalue=greynearp.getRed()
                         pixel_list[nearcol]=[pixel_list[nearcol]]+greynearp
-                 nearmatrix=matrix_3_3(pixel_list[0],pixel_list[1],pixel_list[2])
+                nearmatrix=matrix_3_3(pixel_list[0],pixel_list[1],pixel_list[2])
 
-def draw_image(filename):
-    img=image.Image(filename)
-    win=image.ImageWin(img.getWidth(),img.getHeight())
-    img.setDelay(0)
-
-                img.setPixel(col,row,newpixel)
-            else:
-                img.setPixel(col,row,oldpixel)
-    img.draw(win)
-    win.exitonclick()
-     
 def grayscale(pixel):
     newvalue=(pixel.getRed()+pixel.getGreen()+pixel.getBlue())//3
     newpixel=image.Pixel(newvalue,newvalue,newvalue)
