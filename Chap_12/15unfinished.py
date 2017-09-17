d05516bf762bbafb4602de6be04ab835a72e70ac
Tim Jones <tim.24.jones@gmail.com>
2017-09-16 23:54:20 -0400

    Update 15unfinished.py

--- Chap_12/15unfinished.py
+++ Chap_12/15unfinished.py
@@ -1,8 +1,10 @@
 input_str=input("Please enter a sentence:")
-lower_str=input_str.lower
-mydict={lower_str[0]:1}#error here, does not support indexing
-for i in range(1,len(lower_str)):
-    if lower_str[i] not in mydict:
-        mydict=mydict+{char:1}
+lower_str=input_str.lower()
+mydict={}
+for i in range(len(lower_str)):
+    char=lower_str[i]
+    if char not in mydict:
+        mydict[char]=1
     else:
         mydict[char]=mydict[char]+1
+    print(char,'\t',mydict[char])
