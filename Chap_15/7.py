de191979089fd3358f62e6654c57b44956a88b13
Tim Jones <tim.24.jones@gmail.com>
2017-10-16 22:52:45 -0400

    Update and rename 7unfinished.py to 7.py

---
+++ Chap_15/7.py
@@ -0,0 +1,56 @@
+#copy of Chap 9, exercise 13
+import turtle
+
+def createLSystem(numIters,axiom):
+    startString = axiom
+    endString = ""
+    for i in range(numIters):
+        endString = processString(startString)
+        startString = endString
+
+    return endString
+
+def processString(oldStr):
+    newstr = ""
+    for ch in oldStr:
+        newstr = newstr + applyRules(ch)
+
+    return newstr
+
+def applyRules(ch):
+    newstr = ""
+    if ch == 'L':
+        newstr = '+RF-LFL-FR+'   # Rule 1
+    elif ch== 'R':
+        newstr='-LF+RFR+FL-'
+    else:
+        newstr = ch    # no rules apply so keep the character
+    return newstr
+
+def drawLsystem(aTurtle, instructions, angle, distance):
+    aTurtle.speed(0)
+    for cmd in instructions:
+        if cmd == 'F':
+            aTurtle.forward(distance)
+        elif cmd == 'B':
+            aTurtle.backward(distance)
+        elif cmd == '+':
+            aTurtle.right(angle)
+        elif cmd == '-':
+            aTurtle.left(angle)
+
+def main():
+    inst = createLSystem(4, "L")   # create the string
+    print(inst)
+    t = turtle.Turtle()            # create the turtle
+    wn = turtle.Screen()
+
+    t.up()
+    t.back(200)
+    t.down()
+    t.speed(9)
+    drawLsystem(t, inst, 90, 5)   # draw the picture
+                                  # angle 60, segment length 5
+    wn.exitonclick()
+
+main()
